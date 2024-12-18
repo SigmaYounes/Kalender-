5d3ae8e457844ebb3cdcfdcea0925a6fd3f7acc8
Younes <youneslahmek@gmail.com>
2024-12-18 23:33:12 +0100

    toString methode, damit ein ganzer String für die Ausgabe erstellt wird

--- src/main/java/Veranstaltung.java
+++ src/main/java/Veranstaltung.java
@@ -1,8 +1,9 @@
-import javax.xml.namespace.QName;
+
 import java.time.LocalDate; /* Hier werdem die Klassen LocalDate und LocalTime aus dem Java.time packet importiert
                             *  Dadurch wird später bei den Datums und Zeit Eingabe gewährleistet dass alles richtig
                             *  formatiert ist*/
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Veranstaltung {
     private String name;
@@ -12,9 +13,9 @@ public class Veranstaltung {
     private LocalTime uhrzeit;
     private LocalDate datum;
 
- // Hier wird der Konsruktor initialisiert - Wenn Veranstaltungsobjekt erstellt wird, werden folgende Attribute mitgegeben:
+ // Hier wird der Konstruktor initialisiert - wenn Veranstaltungsobjekt erstellt wird, werden folgende Attribute mitgegeben:
 
-    public Veranstaltung(String Name, String dresscode,String typ,String ort,LocalDate datum, LocalTime uhrzeit) {
+    public Veranstaltung(String name, String dresscode,String typ,String ort,LocalDate datum, LocalTime uhrzeit) {
         this.name = name;
         this.dresscode = dresscode;
         this.typ = typ;
@@ -47,4 +48,12 @@ public class Veranstaltung {
     public LocalDate getDatum() {
         return datum;
     }
+
+    public String toString () {
+       return "Veranstaltungsname:" + name + "\n" + "Dresscode:" + dresscode + "\n" + "Typ:" + typ + "\n" +
+      "Ort:" + ort + "\n" + "Datum:" + datum.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "\n" +
+      "Uhrzeit:" + uhrzeit.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+
 }
